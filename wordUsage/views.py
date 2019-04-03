80ba204c67195509a7364f03d67bfce770b79c76
= <fazbas@gmail.com>
2019-04-03 06:03:46 -0500

    added error handling for beta API access issues

--- wordUsage/views.py
+++ wordUsage/views.py
@@ -17,9 +17,42 @@ from io import BytesIO
 import matplotlib.pyplot as plt
 
 
+def get_response_term(q, after, before):
+    aggs, frequency, size = 'created_utc', 'month', 0
+    parameters = {"q":q,"aggs":aggs, "frequency":frequency, "size":size, "after":after, "before":before}
+    response=requests.get("https://api.pushshift.io/reddit/search/comment/",params=parameters)
+    data = response.json()
+    return data
 
-#def wordUsage(request):
-    #return render(request, 'wordUsage/wordUsage.html', {}) 
+def get_response_all():
+    response = requests.get("https://beta.pushshift.io/reddit/statistics")
+    return response.json()['comment_all_activity']
+
+def get_epoch(aYear, aMonth):
+    utc = pytz.utc
+    return int(utc.localize(datetime(aYear, aMonth, 1, 0, 0)).timestamp())
+
+def get_utc_dt(epoch):
+    local_dt = dt.datetime.fromtimestamp(epoch)
+    utc = pytz.utc
+    utc_dt_aware = local_dt.astimezone(utc)
+    return datetime(utc_dt_aware.year, utc_dt_aware.month, utc_dt_aware.day)
+
+def return_arrays(bucketed_counts, reverse):
+    """takes in a list of dictionaries; returns a pandas dataframe"""
+    try:
+        d1 = list(bucketed_counts[0].keys())
+    except IndexError:
+        raise IndexError('There was an issue accessing the API')
+    d1.sort(reverse = reverse)
+    count, timestamp = d1[1], d1[0]
+    counts, timestamps = [], []
+    for e in bucketed_counts: #here, it means "for e in data_bucketed"
+        counts.append(e[count])
+        timestamps.append(get_utc_dt(e[timestamp]))
+    df = pd.DataFrame()
+    df['Timestamps'], df['Counts'] = timestamps, counts
+    return df
 
 def wordUsage(request):
     # if this is a POST request we need to process the form data
@@ -51,54 +84,15 @@ def wordUsage(request):
 
 #def returnPlot(request):
 def returnPlot(start_mo, start_yr, end_mo, end_yr, t1, t2 = '', t3 = ''):
-    def get_response_term(q, after, before):
-        parameters = {"q":q,"aggs":aggs, "frequency":frequency, "size":size, "after":after, "before":before}
-        response=requests.get("https://api.pushshift.io/reddit/search/comment/",params=parameters)
-        data = response.json()
-        return data
-    
-    def get_response_all():
-        response = requests.get("https://beta.pushshift.io/reddit/statistics")
-        return response.json()['comment_all_activity']
-    
-    def get_epoch(aYear, aMonth):
-        utc = pytz.utc
-        return int(utc.localize(datetime(aYear, aMonth, 1, 0, 0)).timestamp())
-    
-    def get_utc_dt(epoch):
-        local_dt = dt.datetime.fromtimestamp(epoch)
-        utc = pytz.utc
-        utc_dt_aware = local_dt.astimezone(utc)
-        return datetime(utc_dt_aware.year, utc_dt_aware.month, utc_dt_aware.day)
-    
-    def return_arrays(bucketed_counts, reverse):
-        """takes in a list of dictionaries; returns a pandas dataframe"""
-        d1 = list(bucketed_counts[0].keys())
-        d1.sort(reverse = reverse)
-        count, timestamp = d1[1], d1[0]
-        counts, timestamps = [], []
-        for e in bucketed_counts: #here, it means "for e in data_bucketed"
-            counts.append(e[count])
-            timestamps.append(get_utc_dt(e[timestamp]))
-        df = pd.DataFrame()
-        df['Timestamps'], df['Counts'] = timestamps, counts
-        return df
-    
-    #start_mo, start_yr = int(input('enter a start month for the search')), int(input('enter a start year for the search'))
-    #end_mo, end_yr = int(input('enter an end month for the search')), int(input('enter an end year for the search'))
+
     
     after = get_epoch(start_yr, start_mo)
     before = get_epoch(end_yr, end_mo)
     
-    aggs = 'created_utc'
-    frequency = 'month'
-    size = 0
     
-    ''' start of new attempt - to take in three terms'''
     q_tuple = (['"' + t1 + '"'], ['"' + t2 + '"'], ['"' + t3 + '"'])
     counter = 0
     for e in q_tuple:
-        #e.append('"' + input('enter a word or phrase: ') + '"')
         if e[0] == '""':
             break
         else:
@@ -106,10 +100,11 @@ def returnPlot(start_mo, start_yr, end_mo, end_yr, t1, t2 = '', t3 = ''):
             e.append(get_response_term(e[0], after, before)['aggs']['created_utc'])
             if e[1] == []:
                 counter -=1
-    ''' end of new attempt - to take in three terms'''
-    
-    ''' write a left-join loop; run it while q_tuple[-][0] != '""' '''
-    df_all = (return_arrays(get_response_all(), True).groupby(pd.Grouper(key='Timestamps', freq='MS')).sum())
+
+    try:
+        df_all = (return_arrays(get_response_all(), True).groupby(pd.Grouper(key='Timestamps', freq='MS')).sum())
+    except IndexError:
+        return HttpResponse('There was an issue accessing the data. Please try again later.')
     df_term = return_arrays(q_tuple[0][1], True)
     if q_tuple[1][0] != '""' and len(q_tuple[1][1]) > 0:
         df_term = df_term.merge(return_arrays(q_tuple[1][1], True), on = 'Timestamps', how = 'left')#, suffixes=(q_tuple[0][0], q_tuple[1][0]))
@@ -128,32 +123,12 @@ def returnPlot(start_mo, start_yr, end_mo, end_yr, t1, t2 = '', t3 = ''):
     for i in range(counter):
         df_plot[q_tuple[i][0][1:-1]] = df_merged[q_tuple[i][0][1:-1]]/df_merged['Counts']
 
-    #df_plot.plot(x = 'Timestamps')
-    
-    #from #https://django.cowhite.com/blog/creating-charts-and-output-them-as-images-to-the-browser-in-django-using-python-matplotlib-library/
-    # buffer = io.StringIO()
-    # canvas = matplotlib.pyplot.get_current_fig_manager().canvas
-    # canvas.draw()
-    # pilImage = PIL.Image.frombytes("RGB", canvas.get_width_height(), canvas.tostring_rgb())
-    # pilImage.save(buffer, "PNG")
-    # matplotlib.pyplot.close()
-    # return HttpResponse(buffer.getvalue(), mimetype="image/png")
-
-
-
-
-
     fig = plt.figure()
-    #df_plot.plot(x = 'Timestamps')
     df_plot.plot(x = 'Timestamps')
     plt.xlabel('Year')
     plt.ylabel('Percentage of Reddit Comments Containing Term')
     #plt.plot(df_plot)
     figdata = BytesIO()
     plt.savefig(figdata, format='png')
+    print(df_plot)
     return HttpResponse(figdata.getvalue(), content_type="image/png")
-
-
-    #return df_plot
-
-   #https://django.cowhite.com/blog/creating-charts-and-output-them-as-images-to-the-browser-in-django-using-python-matplotlib-library/
